d6404b62b63395f37d12bb6994402fc91263f4b3
Becky DeNardis <rad24@duke.edu>
2012-04-03 23:55:02 -0400

    changed Reflection

--- src/editor/Reflection.java
+++ src/editor/Reflection.java
@@ -13,67 +13,58 @@ import java.util.List;
 import sprite.Behavior;
 
 public class Reflection {
-    /**
-     * Scans all classes accessible from the context class loader which belong to the given package and subpackages.
-     *
-     * @param packageName The base package
-     * @return The classes
-     * @throws ClassNotFoundException
-     * @throws IOException
-     */
+    
     @SuppressWarnings("unchecked")
-    private static List<Class> getClasses(String packageName)
-            throws ClassNotFoundException, IOException 
+    private static List<Class> getClasses(String packageName) throws ClassNotFoundException, IOException 
     {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         assert classLoader != null;
-        String path = packageName.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = new ArrayList<File>();
-        while (resources.hasMoreElements()) {
+        String pathName = packageName.replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources(pathName);
+        List<File> directories = new ArrayList<File>();
+        while (resources.hasMoreElements()) 
+        {
             URL resource = resources.nextElement();
             String fileName = resource.getFile();
             String fileNameDecoded = URLDecoder.decode(fileName, "UTF-8");
-            dirs.add(new File(fileNameDecoded));
+            directories.add(new File(fileNameDecoded));
         }
         ArrayList<Class> classes = new ArrayList<Class>();
-        for (File directory : dirs) {
+        for (File directory : directories) 
+        {
             classes.addAll(findClasses(directory, packageName));
         }
         return classes;
     }
     
-    /**
-     * Recursive method used to find all classes in a given directory and subdirs.
-     *
-     * @param directory   The base directory
-     * @param packageName The package name for classes found inside the base directory
-     * @return The classes
-     * @throws ClassNotFoundException
-     */
+
     @SuppressWarnings("unchecked")
     private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException 
     {
         List<Class> classes = new ArrayList<Class>();
-        if (!directory.exists()) {
+        if (!directory.exists()) 
+        {
             return classes;
         }
         File[] files = directory.listFiles();
-        for (File file : files) {
+        for (File file : files) 
+        {
             String fileName = file.getName();
-            if (file.isDirectory()) {
+            if (file.isDirectory()) 
+            {
                 assert !fileName.contains(".");
                 classes.addAll(findClasses(file, packageName + "." + fileName));
-            } else if (fileName.endsWith(".class") && !fileName.contains("$")) {
-                Class _class;
+            } else if (fileName.endsWith(".class") && !fileName.contains("$")) 
+            {
+                Class c;
                 try {
-                    _class = Class.forName(packageName + '.' + fileName.substring(0, fileName.length() - 6));
+                    c = Class.forName(packageName + '.' + fileName.substring(0, fileName.length() - 6));
                 } catch (ExceptionInInitializerError e) {
 
-                    _class = Class.forName(packageName + '.' + fileName.substring(0, fileName.length() - 6),
+                    c = Class.forName(packageName + '.' + fileName.substring(0, fileName.length() - 6),
                             false, Thread.currentThread().getContextClassLoader());
                 }
-                classes.add(_class);
+                classes.add(c);
             }
         }
         return classes;
@@ -95,14 +86,6 @@ public class Reflection {
     
     public Reflection(){};
     
-//    public static void main(String[] args) throws ClassNotFoundException, IOException
-//    {
-//        Reflection r = new Reflection();
-//        List<Class> list = r.getBehaviors();
-//        for(Class c: list)
-//        {
-//            System.out.println(c);
-//        }
-//    }
+
     
 }
