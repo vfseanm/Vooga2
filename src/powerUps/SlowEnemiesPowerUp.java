e07e4296e026470cb2480b04059b26405b5169b7
Dustin Alin <dma13@duke.edu>
2012-04-06 01:14:42 -0400

    Eliminated warnings

--- src/powerUps/SlowEnemiesPowerUp.java
+++ src/powerUps/SlowEnemiesPowerUp.java
@@ -3,6 +3,10 @@ package powerUps;
 import java.awt.image.BufferedImage
 ;
 
+import attributes.Attribute;
+
+import com.golden.gamedev.Game;
+
 import platforms.BreakablePlatformItemFactory;
 
 import fighter.Fighter;
@@ -10,19 +14,24 @@ import fighter.Fighter;
 @SuppressWarnings("serial")
 public class SlowEnemiesPowerUp extends PowerUp {
 
-    public SlowEnemiesPowerUp(BufferedImage im, double x, double y, String image, Fighter fighter) {
-        super(im, x, y, image, fighter);
+    public SlowEnemiesPowerUp(BufferedImage im, double x, double y, String image, Fighter fighter, Game game) {
+        super(im, x, y, image, fighter, game);
     }
     
-    private SlowEnemiesPowerUp(BufferedImage im, String image, Fighter fighter) {
-        super(im, 0, 0, image, fighter);
+    private SlowEnemiesPowerUp(BufferedImage im, String image, Fighter fighter, Game game) {
+        super(im, 0, 0, image, fighter, game);
     }
     
-    public static BreakablePlatformItemFactory getFactory(BufferedImage im, String image, Fighter fighter) {
-        return new BreakablePlatformItemFactory(new SlowEnemiesPowerUp(im, image, fighter));
+    public static BreakablePlatformItemFactory getFactory(BufferedImage im, String image, Fighter fighter, Game game) {
+        return new BreakablePlatformItemFactory(new SlowEnemiesPowerUp(im, image, fighter, game));
     }
     
     public PowerUp makeItem(double x, double y) {
-        return new SlowEnemiesPowerUp(getImage(), x, y, getImageName(), getFighter());
+        return new SlowEnemiesPowerUp(getImage(), x, y, getImageName(), getFighter(), myGame);
+    }
+    
+    public Attribute getAttribute() {
+        //to be implemented later
+        return null;
     }
 }
--- src/powerUps/SpeedUpPowerUp.java
+++ src/powerUps/SpeedUpPowerUp.java
@@ -2,6 +2,10 @@ package powerUps;
 
 import java.awt.image.BufferedImage;
 
+import com.golden.gamedev.Game;
+
+import attributes.Attribute;
+
 import platforms.BreakablePlatformItemFactory;
 
 import fighter.Fighter;
@@ -9,19 +13,24 @@ import fighter.Fighter;
 @SuppressWarnings("serial")
 public class SpeedUpPowerUp extends PowerUp {
 
-    public SpeedUpPowerUp(BufferedImage im, double x, double y, String image, Fighter fighter) {
-        super(im, x, y, image, fighter);
+    public SpeedUpPowerUp(BufferedImage im, double x, double y, String image, Fighter fighter, Game game) {
+        super(im, x, y, image, fighter, game);
     }
     
-    private SpeedUpPowerUp(BufferedImage im, String image, Fighter fighter) {
-        super(im, 0, 0, image, fighter);
+    private SpeedUpPowerUp(BufferedImage im, String image, Fighter fighter, Game game) {
+        super(im, 0, 0, image, fighter, game);
     }
     
-    public static BreakablePlatformItemFactory getFactory(BufferedImage im, String image, Fighter fighter) {
-        return new BreakablePlatformItemFactory(new SpeedUpPowerUp(im, image, fighter));
+    public static BreakablePlatformItemFactory getFactory(BufferedImage im, String image, Fighter fighter, Game game) {
+        return new BreakablePlatformItemFactory(new SpeedUpPowerUp(im, image, fighter, game));
     }
     
     public PowerUp makeItem(double x, double y) {
-        return new SpeedUpPowerUp(getImage(), x, y, getImageName(), getFighter());
+        return new SpeedUpPowerUp(getImage(), x, y, getImageName(), getFighter(), myGame);
+    }
+    
+    public Attribute getAttribute() {
+        //to be implemented later
+        return null;
     }
 }
