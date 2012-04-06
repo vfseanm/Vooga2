1e6fdbc0a92d949cef839dbcad251e22bd05cf33
Dustin Alin <dma13@duke.edu>
2012-04-05 21:43:25 -0400

    Cleaning up. WOOHOO

--- src/platforms/PlatformDecorator.java
+++
@@ -1,9 +0,0 @@
-package platforms;
-
-public class PlatformDecorator extends AbstractPlatform {
-      private AbstractPlatform myDecoratorComponent;
-      
-      public PlatformDecorator(AbstractPlatform decoratorComponent) {
-            this.myDecoratorComponent = decoratorComponent;
-      }
-}
