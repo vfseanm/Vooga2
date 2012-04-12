4afc3223b7f9a71f95281b1f513d799c0719b909
Alexander Bruce <ajbruce60@gmail.com>
2012-04-11 20:08:49 -0400

    gravity and movement

---
+++ src/attributes/Gravity.java
@@ -0,0 +1,19 @@
+package attributes;
+
+public class Gravity extends Attribute implements Updateable
+{
+
+    public void update (long elaspedTime)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getName ()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/enemies/movement/OneDirectionMovement.java
@@ -0,0 +1,27 @@
+package enemies.movement;
+
+import attributes.Attribute;
+import attributes.Updateable;
+
+public class OneDirectionMovement extends Attribute implements Updateable
+{
+    public OneDirectionMovement(){
+        
+    }
+    public void update (long elaspedTime)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getName ()
+    {
+        return "OneDirectionMovement";
+    }
+    
+    public String toString(){
+        return null;
+    }
+
+}
