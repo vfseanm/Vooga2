e9f656f3ab80b325c68bbf053347429fae0d303d
Ash Sundar <ashwin.sundar@duke.edu>
2012-04-12 04:16:29 -0400

    sdf

--- src/collisions/GameCollisionManager.java
+++ src/collisions/GameCollisionManager.java
@@ -2,9 +2,13 @@ package collisions;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import platforms.SimplePlatform;
 import sprite.*;
 import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.collision.CollisionGroup;
+
+import enemies.Enemy;
 
 
 
