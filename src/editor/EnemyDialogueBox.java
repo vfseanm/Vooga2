78d8c05f724bb6210703e8b0f9eb8e95cbb12d80
Becky DeNardis <rad24@duke.edu>
2012-04-13 03:37:53 -0400

    fixed checkbox issue

--- src/editor/EnemyDialogueBox.java
+++ src/editor/EnemyDialogueBox.java
@@ -103,59 +103,62 @@ public class EnemyDialogueBox extends DialogueBox {
         }
         public void actionPerformed(ActionEvent e)
         {
-            Constructor[] constructors = associatedClass.getConstructors();
-            Constructor constructor = null;
-            for(Constructor c: constructors)
+            if(box.isSelected())
             {
-                if(c.isAnnotationPresent(editorConstructor.class))
+                Constructor[] constructors = associatedClass.getConstructors();
+                Constructor constructor = null;
+                for(Constructor c: constructors)
                 {
-                    constructor = c;
-                }
-            }
-            if(constructor == null)
-            {
-                throw new RuntimeException();
-            }
-            
-            Annotation a = constructor.getAnnotation(editorConstructor.class);
-            String[] paramNames = ((editorConstructor) a).parameterNames();
-            Class[] paramTypes =constructor.getParameterTypes();
-            Object[] argList = null;
-            //System.out.println(paramNames.length);
-            //System.out.println("got here");
-            if(!paramNames[0].equals(""))
-            {
-                argList = new Object[paramNames.length];
-                for(int i=0; i< paramNames.length; i++)
-                {
-                    String selectedValue = JOptionPane
-                        .showInputDialog("What would you like the "+ paramNames[i]+ " to be?");
-                    
-
-                    if(paramTypes[i].equals(int.class))
+                    if(c.isAnnotationPresent(editorConstructor.class))
                     {
-                        argList[i]=Integer.parseInt(selectedValue);
-                    }
-                    if(paramTypes[i].equals(String.class))
-                    {
-                        argList[i] = selectedValue;
-                    }
-                    if(paramTypes[i].equals(double.class))
-                    {
-                        argList[i] = Double.parseDouble(selectedValue);
-                    }
-                    if(paramTypes[i].toString().equals("boolean"))
-                    {
-                        argList[i] = Boolean.parseBoolean(selectedValue);
+                        constructor = c;
                     }
                 }
-            }
+                if(constructor == null)
+                {
+                    throw new RuntimeException();
+                }
+                
+                Annotation a = constructor.getAnnotation(editorConstructor.class);
+                String[] paramNames = ((editorConstructor) a).parameterNames();
+                Class[] paramTypes =constructor.getParameterTypes();
+                Object[] argList = null;
+                //System.out.println(paramNames.length);
+                //System.out.println("got here");
+                if(!paramNames[0].equals(""))
+                {
+                    argList = new Object[paramNames.length];
+                    for(int i=0; i< paramNames.length; i++)
+                    {
+                        String selectedValue = JOptionPane
+                            .showInputDialog("What would you like the "+ paramNames[i]+ " to be?");
+                        
+    
+                        if(paramTypes[i].equals(int.class))
+                        {
+                            argList[i]=Integer.parseInt(selectedValue);
+                        }
+                        if(paramTypes[i].equals(String.class))
+                        {
+                            argList[i] = selectedValue;
+                        }
+                        if(paramTypes[i].equals(double.class))
+                        {
+                            argList[i] = Double.parseDouble(selectedValue);
+                        }
+                        if(paramTypes[i].toString().equals("boolean"))
+                        {
+                            argList[i] = Boolean.parseBoolean(selectedValue);
+                        }
+                    }
+                }
              
                    // Attribute att = (Attribute) constructor.newInstance(argList);
                     List<Object> attribute = new ArrayList<Object>();
                     attribute.add(constructor);
                     attribute.add(argList);
                     attributeInstanceMap.put(box, attribute);
+                }
                 
             }
     }
