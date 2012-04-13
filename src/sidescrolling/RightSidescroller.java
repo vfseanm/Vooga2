e428bde5bbcd26437920b49f6b6519e77a07707d
Dustin Alin <dma13@duke.edu>
2012-04-12 20:40:20 -0400

    Minor changes

--- src/sidescrolling/RightSidescroller.java
+++ src/sidescrolling/RightSidescroller.java
@@ -2,15 +2,16 @@ package sidescrolling;
 
 import java.awt.event.KeyEvent;
 
-import com.golden.gamedev.Game;
+import Tester.Test;
+
 import com.golden.gamedev.object.Sprite;
 
 public class RightSidescroller extends DecoratedSidescroller {
     
     private double rightSpeed;
-    private Game myGame;
+    private Test myGame;
     
-    public RightSidescroller(Game game, Sidescroller scroller, double speed) {
+    public RightSidescroller(Test game, Sidescroller scroller, double speed) {
         super(scroller);
         myGame = game;
         if (speed > 0) {
