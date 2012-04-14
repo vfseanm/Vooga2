c63d05db607640ecbb83a47647e9667500676d82
Ash Sundar <ashwin.sundar@duke.edu>
2012-04-14 16:02:27 -0400

    sdf

--- src/TestGame.java
+++ src/TestGame.java
@@ -45,9 +45,8 @@ public class TestGame extends Game {
         
         
         bob.addAttribute(new Gravity(1));
-        bob.addAttribute(new OneDirectionMovement("left",1));        
-        bob.addAttribute(new JumpingMovement(1,80));
-        //bob.addAttribute(new JumpingMovement(1,100));
+       // bob.addAttribute(new OneDirectionMovement("left",1));        
+        bob.addAttribute(new JumpingMovement(1,90));
         
         counter=0;
         a.clear(); 
