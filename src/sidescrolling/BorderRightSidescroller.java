74cab627e8b0179cd07249a0ae3bfc0ba575f0b9
Dustin Alin <dma13@duke.edu>
2012-04-15 23:23:47 -0400

    Changed names

---
+++ src/sidescrolling/BorderRightSidescroller.java
@@ -0,0 +1,37 @@
+package sidescrolling;
+
+import java.awt.event.KeyEvent;
+
+
+
+import com.golden.gamedev.Game;
+import com.golden.gamedev.object.Sprite;
+
+import fighter.Fighter;
+
+public class BorderRightSidescroller extends BorderSidescroller {
+    
+    private double rightSpeed;
+    private double boundary;
+    
+    public BorderRightSidescroller(Game game, Sidescroller scroller, double speed, double offsetFromRight) {
+        super(game, scroller);
+        myGame = game;
+        if (speed > 0) {
+            throw new RuntimeException("You must choose a negative number.");
+        }
+        rightSpeed = speed;
+        boundary = myGame.getWidth() - offsetFromRight;
+    }
+    
+    public void move(Sprite sprite) {
+        if (myGame.keyDown(KeyEvent.VK_RIGHT)) {
+            Fighter fighter = getFighter();
+            if (fighter.getX() >= boundary) {
+                sprite.moveX(rightSpeed);
+                fighter.setX(boundary);
+            }
+        }
+    }
+    
+}
