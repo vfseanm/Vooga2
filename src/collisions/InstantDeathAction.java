026e84cf1c1a4a5f8d1c0d90126adb90216e9b7b
Ash Sundar <ashwin.sundar@duke.edu>
2012-04-17 02:22:33 -0400

    Hi

--- src/collisions/InstantDeathAction.java
+++ src/collisions/InstantDeathAction.java
@@ -14,7 +14,6 @@ import enemies.Enemy;
 public class InstantDeathAction implements ActionPerformer{
 	
 	public void action(Enemy sprite1, Fighter sprite2, int collisionType) {
-		sprite2.dies();
 	}
 
 	public void action(Sprite sprite1, Sprite sprite2, int collisionType) {
