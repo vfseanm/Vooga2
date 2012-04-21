773619e70042f8d0fbac6eea2410572f108730b6
Ash Sundar <ashwin.sundar@duke.edu>
2012-04-21 14:37:16 -0400

    Hi

--- src/platforms/platformtypes/AbstractPlatform.java
+++ src/platforms/platformtypes/AbstractPlatform.java
@@ -1,13 +1,5 @@
 package platforms.platformtypes;
 import java.util.List;
-
-import collisions.CustomActionPerformer;
-
-import com.golden.gamedev.object.collision.CollisionGroup;
-
-import enemies.Enemy;
-
-
 import java.util.ResourceBundle;
 import sprite.AnimatedGameSprite;
 
@@ -63,29 +55,4 @@ public abstract class AbstractPlatform extends AnimatedGameSprite {
 	
 	public abstract Object clone();
 	
-	
-	public void standardAction (AnimatedGameSprite sprite1, int collisionType){ 	
-		if (collisionType == CollisionGroup.TOP_BOTTOM_COLLISION){
-			if ( (sprite1.getX() >= (this.getX() - sprite1.getWidth() ))
-					&& ((sprite1.getX() + sprite1.getWidth()) <= this.getX()+ this.getWidth() + sprite1.getWidth()) ){
-				sprite1.setY(this.getY()-sprite1.getHeight()-1);
-				if (sprite1 instanceof Enemy){
-					((Enemy)sprite1).restoreOriginalAttribute("JumpingMovement");	
-				}
-			}
-		}
-		
-		if (sprite1 instanceof Enemy){
-			if ((collisionType!=CollisionGroup.TOP_BOTTOM_COLLISION) && (collisionType!=CollisionGroup.BOTTOM_TOP_COLLISION))
-				((Enemy)sprite1).invertAttribute("OneDirectionMovement");
-			if(collisionType == CollisionGroup.BOTTOM_TOP_COLLISION){
-				((Enemy)sprite1).allowAttribute("JumpingMovement", false);
-			}
-		}
-	}
-	
-	public void action (AnimatedGameSprite sprite1, int collisionType, CustomActionPerformer act){
-		this.standardAction (sprite1, collisionType);
-		customAction (sprite1, this, collisionType, act); 
-	}
 }
