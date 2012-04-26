a09ce8ac72987b3dca6f15e3766bd0f33af15681
Alexander Bruce <ajbruce60@gmail.com>
2012-04-26 14:25:44 -0400

    enemyattributes

---
+++ src/attributes/enemyattributes/JumpingMovement.java
@@ -0,0 +1,122 @@
+package attributes.enemyattributes;
+
+import java.lang.reflect.Type;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import editor.editorConstructor;
+import editor.json.AttributeFactory;
+import editor.json.JsonableAttribute;
+import attributes.Attribute;
+import attributes.interfaces.Updateable;
+
+
+// go do isActive and use that
+// turnaround
+@SuppressWarnings("serial")
+public class JumpingMovement extends Attribute implements Updateable, JsonableAttribute
+{
+    private int myDistance;
+    private int myTime;
+    private int time;
+
+
+    @editorConstructor(parameterNames = { "distance", "time" })
+    public JumpingMovement (int distance, int delay)
+    {
+
+        super(distance, delay);
+        myDistance = distance;
+        myTime = delay;
+        time=0;
+        
+        
+    }
+
+    public void modifyJumpingMovement (int distance, int time)
+    {
+        myDistance += distance;
+        myTime += time;
+    }
+
+    public void setActivity(boolean active){
+    	myGameCharacter.allowAttribute("Gravity", true);
+    	isActive=active;
+    }
+    
+    public void update (long elapsedTime)
+    {
+        
+        if (isActive)
+        {
+            if (time <= myTime)
+            {
+                
+                myGameCharacter.setY(myGameCharacter.getY() - myDistance);
+                myGameCharacter.allowAttribute("Gravity", false);
+            }
+            else
+            {
+                
+                myGameCharacter.restoreOriginalAttribute("Gravity");
+            }
+        }
+        
+        time++;
+
+    }
+    //Repeated Code Andrew Help
+    public void invert(){
+        myDistance=-myDistance;
+    }
+
+    @Override
+    public String getName ()
+    {
+        return "JumpingMovement";
+    }
+
+
+    public String toString ()
+    {
+        return "Attribute JumpingMovement my jump distance is " + myDistance +
+               " my jump time is " + myTime+" activity is " +isActive;
+
+    }
+    
+    public Object clone()
+    {
+        return new JumpingMovement(myDistance, myTime);
+    }
+    
+    public String toJson()
+    {
+        Gson gson = new Gson();
+        List<Integer> argList = new ArrayList<Integer>();
+        argList.add(myDistance);
+        argList.add(myTime);
+        return gson.toJson(argList);
+    }
+    
+    public JumpingMovement fromJson(String json)
+    {
+        Gson gson = new Gson();
+        Type collectionType = new TypeToken<List<Integer>>(){}.getType();
+        List<Integer> argList = gson.fromJson(json, collectionType);
+        return new JumpingMovement(argList.get(0), argList.get(1));
+    }
+    
+    private JumpingMovement(){}
+    
+    public static AttributeFactory<JumpingMovement> getFactory()
+    {
+        return new AttributeFactory<JumpingMovement>(new JumpingMovement());
+    }
+   
+
+}
---
+++ src/attributes/enemyattributes/PathFollowingMovement.java
@@ -0,0 +1,122 @@
+package attributes.enemyattributes;
+
+import java.awt.Point;
+
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import editor.editorConstructor;
+import editor.input.inputTypes.Line;
+import editor.json.AttributeFactory;
+import editor.json.JsonableAttribute;
+import attributes.Attribute;
+import attributes.interfaces.Updateable;
+
+
+@SuppressWarnings("serial")
+public class PathFollowingMovement extends Attribute
+    implements Updateable, Cloneable, JsonableAttribute
+{
+    private ArrayList<Point> myPath;
+    private int index;
+    private int increment = 1;
+
+    @editorConstructor(parameterNames = { "" })
+    public PathFollowingMovement (Line path)
+    {
+        super(path);
+        myPath = path.getLine();
+        index = 0;
+        System.out.println("my path:" + myPath);
+    }
+
+
+    public void update (long elaspedTime)
+    {
+        if (isActive)
+        {
+            if (index < myPath.size() && index >= 0)
+            {
+                myGameCharacter.setLocation(myPath.get(index).getX(),
+                                            myPath.get(index).getY());
+            }
+            else
+            {
+                invert();
+            }
+            index += increment;
+
+        }
+
+    }
+
+
+    public void invert ()
+    {
+        increment = -increment;
+
+    }
+
+
+    public void modifyPathFollowingMovement (Point p)
+    {
+        myPath.add(p);
+    }
+
+
+    public void modifyPathFollowingMovement (Point p, int insertIndex)
+    {
+        myPath.add(insertIndex, p);
+    }
+
+
+    @Override
+    public String getName ()
+    {
+
+        return "PathFollowingMovement";
+    }
+
+
+    public String toString ()
+    {
+        return "Attribute PathFollowingMovement my path is " + myPath;
+    }
+
+
+    @Override
+    public Object clone ()
+    {
+       Line l = new Line();
+       l.setLine(myPath);
+       return new PathFollowingMovement(l);
+    }
+    
+    public String toJson()
+    {
+        Gson gson = new Gson();
+        return gson.toJson(myPath);
+    }
+    
+    public PathFollowingMovement fromJson(String json)
+    {
+        Gson gson = new Gson();
+        Type collectionType = new TypeToken<ArrayList<Point>>(){}.getType();
+        ArrayList<Point> path = gson.fromJson(json, collectionType);
+        Line l = new Line();
+        l.setLine(path);
+        return new PathFollowingMovement(l);
+    }
+    
+    
+    private PathFollowingMovement(){};
+    public static AttributeFactory<PathFollowingMovement> getFactory()
+    {
+        return new AttributeFactory<PathFollowingMovement>(new PathFollowingMovement());
+    }
+    
+
+}
