3c166a641e7ac9dd60051681c21932304ae516cb
Alexander Bruce <ajbruce60@gmail.com>
2012-04-26 18:37:59 -0400

    pathing back to absolute

--- src/attributes/enemyattributes/PathFollowingMovement.java
+++ src/attributes/enemyattributes/PathFollowingMovement.java
@@ -1,10 +1,13 @@
 package attributes.enemyattributes;
 
 import java.awt.Point;
+
+
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
 import editor.editorConstructor;
 import editor.input.inputTypes.Line;
 import editor.json.AttributeFactory;
@@ -21,28 +24,13 @@ public class PathFollowingMovement extends Attribute
     private int index;
     private int increment = 1;
 
-
     @editorConstructor(parameterNames = { "" })
     public PathFollowingMovement (Line path)
     {
         super(path);
-        myPath = new ArrayList<Point>();
-        ArrayList<Point> reference = path.getLine();
-
-        for (int i = 0; i < reference.size() - 1; i++)
-        {
-
-            Point first = reference.get(i);
-            Point second = reference.get(i + 1);
-            int x = (int) (second.getX() - first.getX());
-            int y = (int) (second.getY() - first.getY());
-            Point delta = new Point(x, y);
-            myPath.add(delta);
-
-        }
-
+        myPath = path.getLine();
         index = 0;
-
+        System.out.println("my path:" + myPath);
     }
 
 
@@ -52,10 +40,8 @@ public class PathFollowingMovement extends Attribute
         {
             if (index < myPath.size() && index >= 0)
             {
-                Point current = myPath.get(index);
-                double x = myGameCharacter.getX() + current.getX();
-                double y = myGameCharacter.getY() + current.getY();
-                myGameCharacter.setLocation(x, y);
+                myGameCharacter.setLocation(myPath.get(index).getX(),
+                                            myPath.get(index).getY());
             }
             else
             {
@@ -71,16 +57,6 @@ public class PathFollowingMovement extends Attribute
     public void invert ()
     {
         increment = -increment;
-        for (int i = 0; i < myPath.size(); i++)
-        {
-            Point current = myPath.get(i);
-            int x = (int) (current.getX());
-            int y = (int) (current.getY());
-            Point delta = new Point(x, y);
-            myPath.remove(i);
-            myPath.add(i, delta);
-
-        }
 
     }
 
@@ -91,7 +67,7 @@ public class PathFollowingMovement extends Attribute
     }
 
 
-    public void modifyPathFollowingMovement (int insertIndex, Point p)
+    public void modifyPathFollowingMovement (Point p, int insertIndex)
     {
         myPath.add(insertIndex, p);
     }
@@ -114,38 +90,33 @@ public class PathFollowingMovement extends Attribute
     @Override
     public Object clone ()
     {
-        Line l = new Line();
-        l.setLine(myPath);
-        return new PathFollowingMovement(l);
+       Line l = new Line();
+       l.setLine(myPath);
+       return new PathFollowingMovement(l);
     }
-
-
-    public String toJson ()
+    
+    public String toJson()
     {
         Gson gson = new Gson();
         return gson.toJson(myPath);
     }
-
-
-    public PathFollowingMovement fromJson (String json)
+    
+    public PathFollowingMovement fromJson(String json)
     {
         Gson gson = new Gson();
-        Type collectionType = new TypeToken<ArrayList<Point>>()
-        {}.getType();
+        Type collectionType = new TypeToken<ArrayList<Point>>(){}.getType();
         ArrayList<Point> path = gson.fromJson(json, collectionType);
         Line l = new Line();
         l.setLine(path);
         return new PathFollowingMovement(l);
     }
-
-
-    private PathFollowingMovement ()
-    {};
-
-
-    public static AttributeFactory<PathFollowingMovement> getFactory ()
+    
+    
+    private PathFollowingMovement(){};
+    public static AttributeFactory<PathFollowingMovement> getFactory()
     {
         return new AttributeFactory<PathFollowingMovement>(new PathFollowingMovement());
     }
+    
 
 }
