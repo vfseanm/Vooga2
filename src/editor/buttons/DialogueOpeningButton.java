66fdbea2052e264946c98561921ffd4701710d8c
Becky DeNardis <rad24@duke.edu>
2012-04-27 06:35:14 -0400

    start of comments

--- src/editor/buttons/DialogueOpeningButton.java
+++ src/editor/buttons/DialogueOpeningButton.java
@@ -6,12 +6,25 @@ import com.golden.gamedev.gui.TButton;
 import editor.EditorView;
 import editor.dialogues.DialogueBox;
 
-
+/**
+ * Button that opens a dialogue box
+ * @author Sean
+ *
+ */
 public class DialogueOpeningButton extends TButton
 {
     private EditorView myView;
     private DialogueBox myDialogueBox;
-    
+    /**
+     * 
+     * @param name: the name of the button
+     * @param x: the x location of the button
+     * @param y: they y location of the button
+     * @param width: width of box
+     * @param height: height of box
+     * @param view: reference to the level editor view
+     * @param dialogueBox: dialogue box that is opened when the button is clicked
+     */
     public DialogueOpeningButton(String name,
                     int x,
                     int y,
@@ -22,7 +35,10 @@ public class DialogueOpeningButton extends TButton
         myView = view;
         myDialogueBox = dialogueBox;
     }
-
+    
+    /**
+     * Action performed when button is clicked
+     */
     public void doAction ()
     {
         myView.openDialogue(myDialogueBox.clone());
