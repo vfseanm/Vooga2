b6bc6c6cb65329fd655eea7c24afac7e56ad41ea
Tori Reynolds <torireynolds@gmail.com>
2012-04-26 23:22:24 -0400

    Minor refactoring, moved Fighter weapons -> own package in weapons

---
+++ src/weapons/enemyweapons/Fireball.java
@@ -0,0 +1,57 @@
+package weapons.enemyweapons;
+
+import playfield.SingletonSpriteManager;
+import sprite.AnimatedGameSprite;
+import weapons.Weapon;
+
+import com.golden.gamedev.object.Sprite;
+import character.AttributeUser;
+
+
+public class Fireball implements Weapon
+{
+    private AnimatedGameSprite myFireball;
+    private double myDamage;
+    private int myDelay;
+    private int counter;
+
+    //associates a sprite with the attack also needs speed damage and delay between attacks
+    public Fireball (AnimatedGameSprite fireball, double speed, double damage, int delay)
+    {
+        myFireball = fireball;
+        myFireball.setGroup("FIREBALL");
+        myDamage = damage;
+        myDelay = delay;
+        myFireball.setHorizontalSpeed(speed);
+
+    }
+
+    //uses the fireball attack only when the counter allows
+    public void use (AttributeUser character)
+    {
+        if (counter == 0)
+        {
+            SingletonSpriteManager.getInstance().add(myFireball);
+            myFireball.setLocation(character.getX(), character.getY());
+        }
+        else if (counter > myDelay)
+        {
+            counter = 0;
+        }
+        counter++;
+    }
+
+    //useful for collisons to inflict damage
+    public double getDamage ()
+    {
+        return myDamage;
+    }
+
+
+    public void invert ()
+    {
+        myFireball.setHorizontalSpeed(-myFireball.getHorizontalSpeed());
+
+    }
+
+}
---
+++ src/weapons/fighterweapons/FighterTargetedMissile.java
@@ -0,0 +1,72 @@
+package weapons.fighterweapons;
+
+import java.awt.event.KeyEvent;
+
+import java.util.ResourceBundle;
+
+import attributes.Attribute;
+import attributes.interfaces.Input;
+import attributes.interfaces.Updateable;
+
+import character.AttributeUser;
+
+import com.golden.gamedev.engine.BaseInput;
+import com.golden.gamedev.object.*;
+
+import editor.editorConstructor;
+import sprite.*;
+import weapons.Weapon;
+
+@SuppressWarnings("serial")
+public class FighterTargetedMissile implements Weapon, Input{
+
+	transient protected ResourceBundle myGameKeys = ResourceBundle
+    .getBundle("demo.GameKeysResourceBundle");
+	
+	private AnimatedGameSprite 		myMissile;
+	private BaseInput 				myUserInput;
+	private int 					myDamage;
+	private int 					myDelay;
+	private int 					myTimer;
+	private double 					mySpeed;
+	private boolean 				canFire;
+	private int						shootKey;
+
+	
+	public FighterTargetedMissile(AnimatedGameSprite missile, int damage, int delay,
+			double speed) {
+		myMissile = missile;
+		myDamage = damage;
+		myDelay = delay;
+		mySpeed = Math.abs(speed);
+		myTimer = 0;
+		canFire = true;
+		shootKey = Integer.parseInt(myGameKeys.getString("SHOOT"));
+	}
+
+	public void use(AttributeUser character) {
+
+		if (myTimer == 0 && myUserInput.isKeyPressed((shootKey)) && canFire) {
+			SingletonPlayField.getInstance().add(myMissile);
+			myMissile.setLocation(character.getX(), character.getY());
+			
+			// ADD ABOVE METHOD TO LOOP THROUGH ENEMIES & SHOOT AT CLOSEST
+
+			canFire = false;
+		} else if (myTimer > myDelay) {
+			canFire = true;
+			myTimer = 0;
+		}
+		myTimer++; 
+	}
+
+	public void invert() {
+		// USE ABOVE METHOD TO LOOP THROUGH ENEMIES & SHOOT AT FURTHEST AWAY
+	}
+
+	public void setUserInput(BaseInput userInput) {
+		myUserInput = userInput;
+	}
+	
+
+}
