4f790d3d5ef81fc637fecb900df07bd4a6462e5d
Tori Reynolds <torireynolds@gmail.com>
2012-04-27 03:12:56 -0400

    Finished Shooting methods? Added BonusObject collisions & altered Fighter collisions accordingly

--- src/weapons/fighterweapons/FighterTargetedMissile.java
+++ src/weapons/fighterweapons/FighterTargetedMissile.java
@@ -15,36 +15,35 @@ import sprite.*;
 import weapons.Weapon;
 
 @SuppressWarnings("serial")
-public class FighterTargetedMissile implements Weapon, Input{
+public class FighterTargetedMissile implements Weapon, Input {
 
 	transient protected ResourceBundle myGameKeys = ResourceBundle
-    .getBundle("demo.GameKeysResourceBundle");
-	
-	private AnimatedGameSprite 			myMissile;
-	private List<Enemy>					myTargets;
-	private Enemy						myClosestEnemy;
-	private double[]					closestEnemyDistance;
-	private Enemy						myFurthestEnemy;
-	private double[]					furthestEnemyDistance;
-	private BaseInput 					myUserInput;
-	private int 						myDamage;
-	private int 						myDelay;
-	private int 						myTimer;
-	private double 						mySpeed;
-	private boolean						fireAtClosestOrFurthest;
-	private boolean 					canFire;
-	private int							shootKey;
+			.getBundle("demo.GameKeysResourceBundle");
 
-	
-	public FighterTargetedMissile(AnimatedGameSprite missile, int damage, int delay,
-			double speed, boolean value) {
+	private AnimatedGameSprite myMissile;
+	private List<Enemy> myTargets;
+	private Enemy myClosestEnemy;
+	private double[] closestEnemyDistance;
+	private Enemy myFurthestEnemy;
+	private double[] furthestEnemyDistance;
+	private BaseInput myUserInput;
+	private int myDamage;
+	private int myDelay;
+	private int myTimer;
+	private double mySpeed;
+	private boolean fireAtClosest;
+	private boolean canFire;
+	private int shootKey;
+
+	public FighterTargetedMissile(AnimatedGameSprite missile, int damage,
+			int delay, double speed, boolean value) {
 		myMissile = missile;
 		myDamage = damage;
 		myDelay = delay;
 		mySpeed = Math.abs(speed);
 		myTimer = 0;
 		canFire = true;
-		fireAtClosestOrFurthest = value;
+		fireAtClosest = value;
 		shootKey = Integer.parseInt(myGameKeys.getString("SHOOT"));
 	}
 
@@ -53,58 +52,65 @@ public class FighterTargetedMissile implements Weapon, Input{
 		if (myTimer == 0 && myUserInput.isKeyPressed((shootKey)) && canFire) {
 			SingletonSpriteManager.getInstance().add(myMissile);
 			myMissile.setLocation(character.getX(), character.getY());
-			
-			List<Enemy> myEnemies = SingletonSpriteManager.getInstance().getMyEnemies();
-			
 
+			List<Enemy> myEnemies = SingletonSpriteManager.getInstance()
+					.getMyEnemies();
+			
+			if (fireAtClosest) fireAtEnemy(closestEnemyDistance);
+			else fireAtEnemy(furthestEnemyDistance);
+			
 			canFire = false;
-		} 
-		else if (myTimer > myDelay) 
-		{
+		} else if (myTimer > myDelay) {
 			canFire = true;
 			myTimer = 0;
 		}
-		myTimer++; 
+		myTimer++;
 	}
-	
+
 	public double[] calculateDistance(AttributeUser character, Enemy target) {
 		double horizDistance = character.getX() - target.getX();
 		double vertDistance = character.getY() - target.getY();
-		double distance = Math.sqrt(Math.pow(horizDistance, 2) + Math.pow(vertDistance, 2));
+		double distance = Math.sqrt(Math.pow(horizDistance, 2)
+				+ Math.pow(vertDistance, 2));
 		double ratio = vertDistance / horizDistance;
-		double[] calcValues = {distance, ratio, horizDistance};
+		double[] calcValues = { distance, ratio, horizDistance };
 		return calcValues;
 	}
-	
-	public void findClosestAndFurthestEnemies(AttributeUser character, List<Enemy> myEnemies) 
-	{
+
+	public void findClosestAndFurthestEnemies(AttributeUser character,
+			List<Enemy> myEnemies) {
 		closestEnemyDistance = calculateDistance(character, myEnemies.get(0));
 		furthestEnemyDistance = calculateDistance(character, myEnemies.get(0));
-		
-		for (int i = 1; i < myEnemies.size(); i++) 
-		{
-			double[] indexDistanceInfo = calculateDistance(character, myEnemies.get(0));
-			if (indexDistanceInfo[0] < closestEnemyDistance[0])
-			{
+
+		for (int i = 1; i < myEnemies.size(); i++) {
+			double[] indexDistanceInfo = calculateDistance(character, myEnemies
+					.get(0));
+			if (indexDistanceInfo[0] < closestEnemyDistance[0]) {
 				closestEnemyDistance = indexDistanceInfo;
 				myClosestEnemy = myEnemies.get(i);
 			}
-			if (indexDistanceInfo[0] > furthestEnemyDistance[0])
-			{
+			if (indexDistanceInfo[0] > furthestEnemyDistance[0]) {
 				furthestEnemyDistance = indexDistanceInfo;
 				myFurthestEnemy = myEnemies.get(i);
 			}
 		}
 	}
 
-	
+	public void fireAtEnemy(double[] distanceInfo) {
+		if (distanceInfo[2] < 0) {
+			myMissile.setSpeed(-mySpeed, -mySpeed * distanceInfo[1]);
+		} else {
+			myMissile.setSpeed(mySpeed, mySpeed * distanceInfo[1]);
+		}
+		SingletonSpriteManager.getInstance().add(myMissile);
+	}
+
 	public void invert() {
-		fireAtClosestOrFurthest = !fireAtClosestOrFurthest;
+		fireAtClosest = !fireAtClosest;
 	}
 
 	public void setUserInput(BaseInput userInput) {
 		myUserInput = userInput;
 	}
-	
 
 }
