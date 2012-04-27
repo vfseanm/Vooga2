1815f246b94426015b4235ab3c654f9a87bc5536
Alexander Bruce <ajbruce60@gmail.com>
2012-04-27 08:31:11 -0400

    comments

--- src/attributes/Attribute.java
+++ src/attributes/Attribute.java
@@ -1,76 +1,112 @@
 package attributes;
+
 import java.io.Serializable;
-
-
 import character.AttributeUser;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-//import com.sun.tools.internal.xjc.model.Constructor;
+// import com.sun.tools.internal.xjc.model.Constructor;
 import java.lang.reflect.*;
 
 
-
 /**
- * This class is used to allow for dynamic attribute allocation to enemies and fighters.
- * In essence, this allows for flexibility in the framework while still providing
- * functionality by defining some subclasses of Attribute.
- * The enemies and fighter both have a list of Attributes, whose handling is defined in
- * the GameCharacter superclass for uniformity.
+ * This class is used to allow for dynamic attribute allocation to enemies and
+ * fighters. In essence, this allows for flexibility in the framework while
+ * still providing functionality by defining some subclasses of Attribute. The
+ * enemies and fighter both have a list of Attributes, whose handling is defined
+ * in the GameCharacter superclass for uniformity.
  * 
  * @authors Alex & Tori
  */
 @SuppressWarnings("serial")
 public abstract class Attribute implements Serializable, Cloneable
 {
-    protected AttributeUser		myGameCharacter;	
-    protected Object 			myOriginal;
-    protected boolean 			isActive;
-    public static boolean 		makeOriginal = true;   //Note, this is not thread-safe...Sorry multi-threaded programmers
-    
-    public abstract String getName();
-    
-    
-    //associates an attribute with the given enemy/fighter
-    public void setGameCharacter(AttributeUser user) {
+    protected AttributeUser myGameCharacter;
+    protected Object myOriginal;
+    protected boolean isActive;
+    public static boolean makeOriginal = true;
+
+
+    public abstract String getName ();
+
+
+    /**
+     * associates an attribute with the given enemy/fighter
+     * 
+     * @param user the attribute user to be bound to the attribute
+     */
+    public void setGameCharacter (AttributeUser user)
+    {
         myGameCharacter = user;
     }
-    
-    // turns an attribute on/off (active/inactive)
-    public void setActivity(boolean active) {
-    	isActive = active;
+
+
+    /**
+     * sets the isActive of an attribute to the passed parameter of active
+     * 
+     * @param active
+     */
+    public void setActivity (boolean active)
+    {
+        isActive = active;
     }
-    
-    public boolean getActivity() {
-    	return isActive;
+
+
+    /**
+     * returns the activity
+     * 
+     * @return isActive
+     */
+    public boolean getActivity ()
+    {
+        return isActive;
     }
-    
-    public abstract Object clone();
-    
-    public String toJson()
+
+
+    /**
+     * an abstract method to help the user know to implement the clone feature
+     */
+    public abstract Object clone ();
+
+
+    /**
+     * allos a method to help the user to implement Json
+     * 
+     * @return
+     */
+    public String toJson ()
     {
         return "";
     }
-    
 
-    public Attribute (Object...o)
+
+    /**
+     * Constructor for attributes All attributes need to call this super
+     * constructor and pass their parameters to the super constructor this will
+     * create an original version of an attribute which can then later be
+     * restored
+     * 
+     * @param o the parameters of the subclass constructor
+     */
+    public Attribute (Object ... o)
     {
-        isActive=true;
+        isActive = true;
         if (makeOriginal == true)
         {
             makeOriginal = false;
-           
+
             Constructor<?>[] c = this.getClass().getConstructors();
-           
+
             try
             {
-            	
-                for(@SuppressWarnings("rawtypes") Constructor constructor: c)
+
+                for (@SuppressWarnings("rawtypes")
+                Constructor constructor : c)
                 {
-                    if(constructor.getModifiers()==Modifier.PUBLIC)
-                    {    
-                        if(o.length == constructor.getParameterTypes().length)
+                    if (constructor.getModifiers() == Modifier.PUBLIC)
+                    {
+                        if (o.length == constructor.getParameterTypes().length)
                         {
-                            myOriginal =  constructor.newInstance(o);
+                            myOriginal = constructor.newInstance(o);
                             break;
                         }
                     }
@@ -79,27 +115,23 @@ public abstract class Attribute implements Serializable, Cloneable
             }
             catch (IllegalArgumentException e)
             {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
             catch (InstantiationException e)
             {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
             catch (IllegalAccessException e)
             {
-                // TODO Auto-generated catch block
+
                 e.printStackTrace();
             }
             catch (InvocationTargetException e)
             {
-                // TODO Auto-generated catch block
-            	
-            	System.out.println("hello");
+
                 e.printStackTrace();
             }
-            
+
         }
         else
         {
@@ -108,29 +140,31 @@ public abstract class Attribute implements Serializable, Cloneable
     }
 
 
+    /**
+     * this method is used to restore all attributes to their original state
+     * only works if the super constructor is properly called in the subclass
+     */
     public void restoreOriginalAttribute ()
     {
-        isActive=true;
+        isActive = true;
         Field[] myFields = this.getClass().getDeclaredFields();
-        Field[] myOriginalFields =  myOriginal.getClass().getDeclaredFields();
+        Field[] myOriginalFields = myOriginal.getClass().getDeclaredFields();
         for (int i = 0; i < myOriginalFields.length; i++)
         {
             try
             {
-                
+
                 myFields[i].setAccessible(true);
                 myOriginalFields[i].setAccessible(true);
                 myFields[i].set(this, myOriginalFields[i].get(myOriginal));
-                
+
             }
             catch (IllegalArgumentException e)
             {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
             catch (IllegalAccessException e)
             {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
