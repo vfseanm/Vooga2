057e20a2753cdbde9d64412f076103723d804ed0
Alexander Bruce <ajbruce60@gmail.com>
2012-04-27 08:53:08 -0400

    comments

--- src/playfield/SingletonSpriteManager.java
+++ src/playfield/SingletonSpriteManager.java
@@ -5,24 +5,26 @@ import java.util.ArrayList;
 import java.util.List;
 import sprite.AnimatedGameSprite;
 import com.golden.gamedev.object.Sprite;
-
 import enemies.Enemy;
 import fighter.Fighter;
 
 
 /**
- * Singleton Pattern to use for playfields instead of passing them around
- * everywhere
+ * Singleton Pattern to use for this class instead of passing them around
+ * everywhere this class handles the updating and rendering of all sprites
  * 
  * @author Alex
  */
 public class SingletonSpriteManager
 {
-    private List<AnimatedGameSprite> 	mySprites;
-    private List<Enemy>					myEnemies;
-    private List<AnimatedGameSprite> 	toAdd;
+    private List<AnimatedGameSprite> mySprites;
+    private List<Enemy> myEnemies;
+    private List<AnimatedGameSprite> toAdd;
 
 
+    /**
+     * singleton pattern
+     */
     private SingletonSpriteManager ()
     {
         mySprites = new ArrayList<AnimatedGameSprite>();
@@ -31,6 +33,9 @@ public class SingletonSpriteManager
 
     }
 
+    /**
+     * singleton pattern
+     */
     private static class SingletonPlayFieldHolder
     {
         public static final SingletonSpriteManager instance =
@@ -38,12 +43,21 @@ public class SingletonSpriteManager
     }
 
 
+    /**
+     * singleton pattern
+     */
     public static SingletonSpriteManager getInstance ()
     {
         return SingletonPlayFieldHolder.instance;
     }
 
 
+    /**
+     * adds a sprite to toAdd during an update call toAdd will be added to the
+     * mySprites used to prevent concurrent modification
+     * 
+     * @param sprite a sprite to add
+     */
     public void add (Sprite sprite)
     {
 
@@ -56,6 +70,11 @@ public class SingletonSpriteManager
     }
 
 
+    /**
+     * renders all my sprites
+     * 
+     * @param pen the GTGE render
+     */
     public void render (Graphics2D pen)
     {
 
@@ -66,6 +85,11 @@ public class SingletonSpriteManager
     }
 
 
+    /**
+     * updates all the sprites also adds the toAdd list to mySprites
+     * 
+     * @param time
+     */
     public void update (long time)
     {
         if (toAdd.size() > 0)
@@ -73,10 +97,10 @@ public class SingletonSpriteManager
             for (AnimatedGameSprite s : toAdd)
             {
                 mySprites.add(s);
-                
-                if (s instanceof Enemy) 
+
+                if (s instanceof Enemy)
                 {
-                	myEnemies.add((Enemy) s);
+                    myEnemies.add((Enemy) s);
                 }
 
             }
@@ -89,18 +113,33 @@ public class SingletonSpriteManager
     }
 
 
+    /**
+     * returns all the sprites in mySprites
+     * 
+     * @return mySprites
+     */
     public List<AnimatedGameSprite> getMySprites ()
     {
         return mySprites;
     }
 
 
+    /**
+     * sets mySprites to a given list useful for loading in levels and adding
+     * the sprites
+     * 
+     * @param list
+     */
     public void setMySprites (List<AnimatedGameSprite> list)
     {
         mySprites = list;
     }
-    
-    public List<Enemy> getMyEnemies()
+
+    /**
+     * returns the enemies
+     * @return the list of enemies in the game
+     */
+    public List<Enemy> getMyEnemies ()
     {
         return myEnemies;
     }
