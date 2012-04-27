1361c7483c372c89732076a120aa9d653a903ae7
Becky DeNardis <rad24@duke.edu>
2012-04-27 05:52:02 -0400

    commit

--- src/editor/Framework.java
+++ src/editor/Framework.java
@@ -215,6 +215,7 @@ public class Framework implements Serializable {
         factories.add(BonusObject.getFactory());
         factories.add(WildAndCrazyObject.getFactory());
         
+        
         AnimatedGameSprite prototype = null;
         for(SpriteFactory factory: factories)
         {
